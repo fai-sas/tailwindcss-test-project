32bb8485f3170ae2357b95f14ed25a715d1befb8
fai-sas <faisal.diabolic@gmail.com>
2021-07-27 14:33:20 +0600

    card setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     debugScreens: {
       position: ['top', 'left'],
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: ['Oswald'],
+      },
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      },
+    },
   },
   variants: {
     extend: {},
